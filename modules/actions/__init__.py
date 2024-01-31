7ed5151983aebb0c82012f166795fdc4a87f22ec
Huaben <chenhuaben@westlake.edu.cn>
2024-01-31 10:10:54 +0800

    feat: Update __init__ files for role and action module

--- modules/actions/__init__.py
+++ modules/actions/__init__.py
@@ -1,20 +1,26 @@
 from modules.actions.action import Action
 from modules.actions.debug_error import DebugError
-from modules.actions.rephrase_command import RephraseCommand
+from modules.actions.rephrase_command import WritePrompt
 from modules.actions.run_code import RunCode
 from modules.actions.write_code import WriteCode
-from modules.actions.write_test import WriteTest
+from modules.actions.write_test import WriteUnitTest
 from modules.actions.add_command import UserCommand
 from modules.actions.write_design import WriteDesign
 from modules.actions.write_run import WriteRun
+from modules.actions.rewrite_code import RewriteCode
+from modules.actions.rewrite_test import RewriteUnitTest
 
 
 __all__ = [
     "Action",
     "DebugError",
-    "RephraseCommand",
+    "WritePrompt",
     "RunCode",
     "WriteCode",
-    "WriteTest",
-    "UserCommand"
+    "WriteUnitTest",
+    "UserCommand",
+    "WriteDesign",
+    "WriteRun",
+    "RewriteCode",
+    "RewriteUnitTest"
 ]
