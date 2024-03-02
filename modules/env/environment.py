a306986a813fb3a587e03ec43b02663fc7c7ba6e
derrick <2287924124@qq.com>
2024-03-02 15:57:20 +0800

    fix: optimize simulation environment to support large-scale

--- modules/env/environment.py
+++ modules/env/environment.py
@@ -2,36 +2,63 @@ import numpy as np
 import rospy
 import matplotlib.pyplot as plt
 from std_msgs.msg import Float32MultiArray
-from robot import robots
 from std_srvs.srv import SetBool, SetBoolResponse
-from matplotlib.animation import FuncAnimation
+
+from modules.env.robot import Robots, Leader
+from modules.utils import generate_video_from_frames
 
 
 class Env:
-    def __init__(self, robot_list: list, size=(10, 10)):
+    def __init__(
+            self,
+            size=(10, 10),
+            n_robots=3,
+            dt=0.01,
+            if_leader=False,
+            leader_speed=2.0,
+            render_interval=10,
+
+    ):
         rospy.init_node('env_node', anonymous=True)
         self._size = size
-        self._dt = 0.05
-        self._robots = robot_list
-        self._robots_initial_positions = [robot.position.copy() for robot in self._robots]
+        self._dt = dt
+        self._render_interval = render_interval
+        self._leader_speed = leader_speed
+        self._robots = Robots(n_robots, self._size)
+        if if_leader:
+            self._leader = Leader(initial_position=(0, 0))
+            self._leader_publisher = rospy.Publisher('/leader/position', Float32MultiArray, queue_size=1)
+        else:
+            self._leader = None
+        self._robots_initial_positions = self._robots.positions.copy()
+        self._position_publisher = rospy.Publisher('/robots/position', Float32MultiArray, queue_size=1)
+        self._reset_service = rospy.Service('/reset_environment', SetBool, self.reset_environment_callback)
+        rospy.set_param('robots_num', n_robots)
+        rospy.Subscriber('/robots/velocity', Float32MultiArray, self.velocity_callback, callback_args=self._robots)
+
+        # flag to indicate if the test is running,if running, the robots will save their positions to history
+        self._run_test = False
+        # flag to indicate if the frames should be rendered
         self._render_frames = False
         self._fig, self._ax = plt.subplots()
-        self._total_history = []
-        self._position_publishers = []
-        self._run_test = False
-        self._reset_service = rospy.Service('/reset_environment', SetBool, self.reset_environment)
-        for robot in self._robots:
-            rospy.Subscriber(f'/robot_{robot.robot_id}/velocity', Float32MultiArray, self.velocity_callback,
-                             callback_args=robot)
-            self._position_publishers.append(
-                rospy.Publisher(f'/robot_{robot.robot_id}/position', Float32MultiArray, queue_size=10))
+
+        # counter for total run time
+        self._run_time = 0
 
     @staticmethod
-    def velocity_callback(data, robot):
-        print(f"Received velocity for robot {robot.robot_id}: {data.data}")
-        robot.velocity = np.array(data.data)
+    def velocity_callback(data, robots):
+        """
+        velocity_callback is a callback function for the velocity topic.
+        """
+        print("Received velocity")
+        robots.velocities = np.array(data.data).reshape(-1, 2)
 
-    def reset_environment(self, req):
+    def reset_environment_callback(self, req):
+        """
+        reset_environment_callback is a callback function for the reset_environment service.
+        It resets the environment to its initial state and returns a success message.
+        must be called before and after running the script
+        """
         self.reset()
         self._render_frames = req.data
         print("set render frames to", self._render_frames)
@@ -42,75 +69,60 @@ class Env:
         return response
 
     def reset(self):
+        """
+        Reset the environment to its initial state.
+        """
         self._run_test = not self._run_test
-        round_history = []
         if not self._run_test:
-            for i, robot in enumerate(self._robots):
-                robot.position = self._robots_initial_positions[i].copy()
-                robot.velocity = np.array([0, 0], dtype=float)
-                round_history.append(robot.history)
-                robot.history = [robot.position.copy()]
-            self._total_history.append(round_history)
-        print("Environment reset successful!")
-        if self._run_test:
-            print("Test started!")
-        else:
+            self._robots.positions = self._robots_initial_positions.copy()
+            self._robots.velocities = np.zeros_like(self._robots.velocities)
+            self._robots.history = [self._robots.positions.copy()]
+            data_path = rospy.get_param('data_path', '.')
+            generate_video_from_frames(frames_folder=f'{data_path}/frames', video_path=f'{data_path}/video.mp4')
             print("Test stopped! Waiting for new test...")
+            print("Environment reset successful!")
+        else:
+            print("Test started!")
 
     def step(self):
         if self._run_test:
-            for robot in self._robots:
-                new_position = robot.position + robot.velocity * self._dt
-                robot.position = np.clip(new_position, -np.array(self._size) / 2, np.array(self._size) / 2)
-                robot.history.append(robot.position.copy())
-                self._position_publishers[robot.robot_id].publish(Float32MultiArray(data=robot.position.tolist()))
-
-        if self._render_frames:
-            self.render()
+            if self._leader:
+                self._leader.move(self._leader_speed, self._dt)
+                self._leader_publisher.publish(Float32MultiArray(data=self._leader.position))
+            self._robots.move_robots(self._dt)
+            self._position_publisher.publish(Float32MultiArray(data=self._robots.positions.flatten().tolist()))
+            self._run_time += 1
+            if self._run_time % self._render_interval == 0:
+                self.render()
         elif plt.get_fignums():
             plt.close(self._fig)
 
     def render(self):
         if not plt.get_fignums():
             self._fig, self._ax = plt.subplots()
-            plt.show(block=False)
         self._ax.clear()
-        for robot in self._robots:
-            history = np.array(robot.history)
-            self._ax.plot(history[:, 0], history[:, 1], label=f"Robot {robot.robot_id} path")
-            self._ax.plot(history[-1, 0], history[-1, 1], 'o', label=f"Robot {robot.robot_id} current")
-        half_x = int(self._size[0] / 2)
-        half_y = int(self._size[1] / 2)
-        self._ax.set_xlim(-half_x, half_x)
-        self._ax.set_ylim(-half_y, half_y)
-        # self._ax.legend(loc='upper right')
-        plt.draw()
-        plt.pause(0.001)  # This is necessary for the plot to update
 
-    def save_animation(self, histories, file_name):
-        print("Generating animation from robot history...")
-        fig, ax = plt.subplots()
-
-        histories = np.array(histories)
-        frames = histories.shape[1]
-        robots_num = histories.shape[0]
-
-        def update_frame(frame_number):
-            ax.clear()
-            half_x = int(self._size[0] / 2)
-            half_y = int(self._size[1] / 2)
-            ax.set_xlim(-half_x, half_x)
-            ax.set_ylim(-half_y, half_y)
-            for i in range(robots_num):
-                ax.plot(histories[i, :frame_number, 0], histories[i, :frame_number, 1],
-                        label=f"Robot {i}")
-                ax.plot(histories[i, frame_number, 0], histories[i, frame_number, 1], 'o')
-
-        ani = FuncAnimation(fig, update_frame, frames=frames, interval=100)
-        DATA_PATH = rospy.get_param('data_path', '.')
-        ani.save(f'{DATA_PATH}/animation_{file_name}.gif', writer='pillow')
-        print(f"Animation saved as animation_{file_name}.gif!")
-        plt.close(fig)
+        traj_len, robot_num, _ = self._robots.history.shape
+        for i in range(robot_num):
+            show_len = min(10, traj_len)
+            self._ax.plot(self._robots.history[-show_len:, i, 0],
+                          self._robots.history[-show_len:, i, 1],
+                          label=f"Robot {i} path")
+            self._ax.plot(self._robots.history[-1, i, 0],
+                          self._robots.history[-1, i, 1],
+                          'o',
+                          label=f"Robot {i} position")
+        self._ax.plot(self._leader.position[0], self._leader.position[1],
+                      marker='*', markersize=12, color='r',
+                      linestyle='None', label="Leader position")
+        self._ax.set_xlim(-self._size[0], self._size[0])
+        self._ax.set_ylim(-self._size[1], self._size[1])
+        if self._render_frames:
+            plt.draw()
+            plt.pause(0.001)  # This is necessary for the plot to update
+        data_path = rospy.get_param('data_path', '.')
+        frame_id = len(self._robots.history)
+        plt.savefig(f'{data_path}/frames/{frame_id}.png')
 
     def run(self):
         print("Environment started!")
@@ -118,11 +130,9 @@ class Env:
         while not rospy.is_shutdown():
             self.step()
             rate.sleep()
-        for i, history in enumerate(self._total_history):
-            self.save_animation(history, str(i))
         print("Environment stopped!")
 
 
 if __name__ == "__main__":
-    env = Env(robots)
+    env = Env(if_leader=True, n_robots=100, size=(10, 10))
     env.run()
--- modules/env/functions.py
+++ modules/env/functions.py
@@ -1,66 +1,72 @@
 import numpy as np
-
 import rospy
 from std_msgs.msg import Float32MultiArray
-from robot import robots
 
 ros_initialized = False
-velocity_publishers = {}
+velocity_publisher: rospy.Publisher
+robots_num: int
+robots_velocity: np.ndarray
+robots_position: np.ndarray
+timer: rospy.Timer
+
+
+def position_callback(msg):
+    global robots_position
+    robots_position = np.array(msg.data).reshape(-1, 2)
 
 
 def initialize_ros_node():
-    global ros_initialized, velocity_publishers
+    global ros_initialized, velocity_publisher, robots_num, robots_velocity, timer, robots_position
     if not ros_initialized:
         rospy.init_node('robot_control_node', anonymous=True)
         ros_initialized = True
-        robot_ids = get_all_robot_ids()
-        for robot_id in robot_ids:
-            velocity_publishers[robot_id] = rospy.Publisher(f'/robot_{robot_id}/velocity', Float32MultiArray,
-                                                            queue_size=10)
+        velocity_publisher = rospy.Publisher('/robots/velocity', Float32MultiArray, queue_size=10)
+        rospy.Subscriber('/robots/position', Float32MultiArray, position_callback)
+        robots_position = np.array(rospy.wait_for_message('/robots/position', Float32MultiArray).data).reshape(-1, 2)
+        robots_num = rospy.get_param('/robots_num')
+        robots_velocity = np.zeros((robots_num, 2), dtype=float)
+        timer = rospy.Timer(rospy.Duration(0.01), publish_all_velocities)
+
+
+def publish_all_velocities(event):
+    global robots_velocity, velocity_publisher
+    velocity_msg = Float32MultiArray(data=robots_velocity.flatten().tolist())
+    velocity_publisher.publish(velocity_msg)
 
 
 def get_robot_position_by_id(robot_id):
     """
-    Get the position of a robot with the given ID using ROS topic subscription.
+    Get the position of a robot with the given ID.
 
     Parameters:
     - robot_id (int): The ID of the robot to retrieve.
 
     Returns:
     - numpy.ndarray: The position of the robot.
-
-    Raises:
-    - ValueError: If no robot with the specified ID is found.
     """
+    global robots_position
     initialize_ros_node()
-    for robot in robots:
-        if robot.robot_id == robot_id:
-            position_msg = rospy.wait_for_message(f'/robot_{robot_id}/position', Float32MultiArray)
-            return np.array(position_msg.data)
-    raise ValueError(f"Robot with ID {robot_id} not found")
+    positions = robots_position
+    if robot_id >= positions.shape[0]:
+        raise ValueError(f"Robot with ID {robot_id} not found")
+    return positions[robot_id]
 
 
 def set_robot_velocity_by_id(robot_id, velocity):
     """
-    Set the velocity of a robot with the given ID using ROS topic publication.
+    Set the velocity of a robot with the given ID.
 
     Parameters:
     - robot_id (int): The ID of the robot to set the velocity for.
     - velocity (numpy.ndarray): The new velocity to set.
-
-    Raises:
-    - ValueError: If no robot with the specified ID is found.
     """
     initialize_ros_node()  # 确保ROS节点已初始化
     velocity = np.array(velocity, dtype=float)
-    for robot in robots:
-        if robot.robot_id == robot_id:
-            # 使用ROS发布机器人速度
-            velocity_msg = Float32MultiArray(data=velocity.tolist())
-            velocity_publisher = velocity_publishers[robot_id]
-            velocity_publisher.publish(velocity_msg)
-            return
-    raise ValueError(f"Robot with ID {robot_id} not found")
+    if robot_id >= robots_num:
+        raise ValueError(f"Robot with ID {robot_id} not found")
+    if velocity.shape != (2,):
+        raise ValueError(f"Expected velocity shape (2,), got {velocity.shape}")
+    robots_velocity[robot_id] = velocity
 
 
 def get_all_robot_ids():
@@ -70,4 +76,31 @@ def get_all_robot_ids():
     Returns:
     - list: A list containing all the robot IDs.
     """
-    return [robot.robot_id for robot in robots]
+    global robots_num
+    initialize_ros_node()
+
+    return list(range(robots_num))
+
+
+def get_robots_count():
+    """
+    Get the total number of robots.
+
+    Returns:
+    - int: The total number of robots.
+    """
+    global robots_num
+    initialize_ros_node()
+    return robots_num
+
+
+def get_leader_position():
+    """
+    Get the position of the leader robot.
+
+    Returns:
+    - numpy.ndarray: The position of the leader robot.
+    """
+    initialize_ros_node()
+    position_msg = rospy.wait_for_message('/leader/position', Float32MultiArray)
+    return np.array(position_msg.data)
