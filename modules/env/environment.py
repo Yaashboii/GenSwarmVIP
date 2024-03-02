fbb43502e8f3aecb1059064f22f77c36df7a65ab
miang <2594416505@qq.com>
2024-03-02 22:00:22 +0800

    bug: add project root

--- modules/env/environment.py
+++ modules/env/environment.py
@@ -1,3 +1,8 @@
+import sys
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(project_root)
+
 import numpy as np
 import rospy
 import matplotlib.pyplot as plt
