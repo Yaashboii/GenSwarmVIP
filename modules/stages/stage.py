028e22f72048f91ddffe79d608b62b8ccf0c2d1d
miang <2594416505@qq.com>
2024-03-04 21:25:07 +0800

    feat: store Stage|Prompt|Result in log.md

--- modules/stages/stage.py
+++ modules/stages/stage.py
@@ -34,6 +34,7 @@ class Stage(ABC, BaseModel):
     @final
     def run(self) -> StageResult:
         self._logger.info(f"Current stage: {self}")
+        self._context.log.message = f"***\n# *Current stage: {self}*\n"
         return self._run()
 
     def _run(self) -> StageResult:
