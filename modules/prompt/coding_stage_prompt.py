8414e6484a12aec65569812ee80d770401629f42
LufengXU <xulufeng@westlake.edu.cn>
2024-03-12 15:53:33 +0800

    Reorganize the prompt using Markdown syntax

--- modules/prompt/coding_stage_prompt.py
+++ modules/prompt/coding_stage_prompt.py
@@ -1,50 +1,59 @@
 WRITE_FUNCTION_PROMPT_TEMPLATE = """
-These are the environment description: 
+## These are the environment description: 
 {env_des}
 
-These are the basic Robot APIs:
+## These are the basic Robot APIs:
+```python
 {robot_api}
+```
 
-These are existing functions:
+## These are existing functions:
+```python
 {other_functions}
+```
 
+## Constraints: 
 The functions you generate need to comply with the following constraints.
-constrains: 
 1. Above functions are in the same py file as yours and can be invoked directly using their function names.
 2. COMPLETE CODE: Your code will be part of the entire project, so please implement complete, reliable, reusable code snippets.
 3. Set default value: If there is any setting, ALWAYS SET A DEFAULT VALUE, ALWAYS USE STRONG TYPE AND EXPLICIT VARIABLE. 
-5. Before using a others functions/modules, make sure to import them first.
-6. YOU MUST FOLLOW "Data structures and interface definitions". DONT CHANGE ANY DESIGN.
-7. You only need to make sure single function has correct input and output. don't need to consider complex conditions.
-8. You can only complete this one function; you cannot generate other Helper functions. If necessary, you can define functions within this function.
+4. Before using a others functions/modules, make sure to import them first.
+5. YOU MUST FOLLOW "Data structures and interface definitions". DONT CHANGE ANY DESIGN.
+6. You only need to make sure single function has correct input and output. don't need to consider complex conditions.
+7. You can only complete this one function; you cannot generate other Helper functions. If necessary, you can define functions within this function.
 
+## Task description:
 Please finish the following function, maximizing the reuse of existing functions:
 {function}
 """.strip()
 
 WRITE_RUN_PROMPT_TEMPLATE = """
+## Task description:
 You are a robot, you need to translate the sequence diagram into Python code.
-These are the environment description: 
+
+## These are the environment description: 
 {env_des}
 
-The list of functions you can call is as follows:
+## The list of functions you can call is as follows:
+```python
 from functions imports *
 {robot_api}
 {function_list}
+```
 
-constrains: 
+## This is the Sequence Diagram: 
+{sequence_diagram}
+
+## Constrains: 
 1. the functions provided are available, you can use them by "from functions import *" 
 2. Set default value: If there is any setting, ALWAYS SET A DEFAULT VALUE, ALWAYS USE STRONG TYPE AND EXPLICIT VARIABLE. 
 3. Ensure the generated code contains a 'if __name__ == '__main__':' statement and can be executed directly
 
-The generated result should be in the following fields:
-explanation: think step by step. How to translate the sequence diagram into Python code.
-python code: Translate the sequence diagram into corresponding Python code. ```python\n <your response>```
+## The generated result should be in the following fields:
+1. explanation: think step by step. How to translate the sequence diagram into Python code.
+2. python code: Translate the sequence diagram into corresponding Python code. ```python\n <your response>```
 
-The output  TEXT format is as follows:
-explanation: <explanation>
-python code: <python code>
-
-This is the Sequence Diagram: 
-{sequence_diagram}
+## The output  TEXT format is as follows:
+1. explanation: <explanation>
+2. python code: <python code>
 """.strip()
--- modules/prompt/design_stage_prompt.py
+++ modules/prompt/design_stage_prompt.py
@@ -1,56 +1,71 @@
 DesignFunction_PROMPT_TEMPLATE = """
+## Task description:
 In order to assist users in automating specific tasks, you need to design a series of decoupled Python functions based on user functional requirements and constraints.
-- These functions should be decoupled from each other. 
-- Each function can utilize the existing APIs.
-- Each function should have strong reusability and should have a sufficient number of inputs and outputs.
-- Each function does not need to provide the content of the function body; just giving a `pass` is suffice.
 
+## User functional requirements:
+{analysis}
+
+## Constraints:
+1. These functions should be decoupled from each other. 
+2. Each function can utilize the existing APIs.
+3. Each function should have strong reusability and should have a sufficient number of inputs and outputs.
+4. Each function does not need to provide the content of the function body; just giving a `pass` is suffice.
+5. To avoid complexity, functions only need to provide the function name and docstring; you cannot write out the function body. You only need to design rather than write code.
+6. Every function should provide a detailed description of its functionality as well as any constraints that may need to be considered.
+
+## Existing robot APIs:
 These are the existing robot APIs, functions should try to reuse them as much as possible and refer to the design of them:
+```python
 {code}
+```
 
-These are the environment description:
+## These are the environment description:
 {env_des}
 
-The generated result should be in the following fields:
-explanation: think step by step. How do you make the functions conform to the algorithm?
-function list: write the function list in the following format, ```python\n<your python code>```
-- To avoid complexity, functions only need to provide the function name and docstring; you cannot write out the function body. You only need to design rather than write code.
-- Every function should provide a detailed description of its functionality as well as any constraints that may need to be considered.
+## The generated result should be in the following fields:
+1. explanation: think step by step. How do you make the functions conform to the algorithm?
+2. function list: write the function list in the following format, ```python\n<your python code>```
 
-The output TEXT format is as follows:
-explanation: <explanation>
-function list: <function list>"
-
-Analysis: {analysis}
+## The output TEXT format is as follows:
+1. explanation: <explanation>
+2. function list: <function list>
 """.strip()
 
 WriteSeqDiagram_PROMPT_TEMPLATE = """
+## Task description:
 Based on the user requirements document and current functions, you need to design a sequence diagram.
-These are the environment description:
+
+## User requirements: 
+{analysis}
+
+## These are the environment description:
 {env_des}
 
-These are the basic Robot APIs:
+## These are the basic Robot APIs:
+```python
 {robot_api}
+```
 
-These are existing functions:
+## These are existing functions:
+```python
 {function_list}
+```
 
-Explanation: 
+## Explanation: 
 1. The sequence diagram is to call the existing functions to fulfill the user's requirements.
 2. The sequence diagram can be directly translated into Python code, which is capable of continuously monitoring the environment and outputting control signals at a certain frequency.
 3. The code generated from the sequence diagram should not loop infinitely; it should be able to exit the loop once the task is completed.
-constrains: 
+
+## Constraints: 
 1. You need to use these existing functions to generate a call flow diagram to fulfill the user's requirements.
 2. You can't define any new functions.
 3. Ensure that the sequence diagram translates directly into executable code that can accomplish the task objectives.
 
-The generated result should be in the following fields:
-explanation: think step by step. How do you make the diagram conform to the algorithm?
-sequence diagram: use Mermaid's sequenceDiagram to write sequence diagram, ```mermaid\nsequenceDiagram\n <your response>```
+## The generated result should be in the following fields:
+1. explanation: think step by step. How do you make the diagram conform to the algorithm?
+2. sequence diagram: use Mermaid's sequenceDiagram to write sequence diagram, ```mermaid\nsequenceDiagram\n <your response>```
 
-The output TEXT  format is as follows:
-explanation: <explanation>
-sequence diagram: <sequence diagram>
-
-User requirements: {analysis}
+## The output TEXT  format is as follows:
+1. explanation: <explanation>
+2. sequence diagram: <sequence diagram>
 """.strip()
