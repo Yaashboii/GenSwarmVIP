0d4cedcb350d86d8c44364a73f58525f5889877d
derrick <2287924124@qq.com>
2024-03-13 09:28:48 +0800

    fix: add usage example in robot api prompt and remove leader api

--- modules/prompt/robot_api_prompt.py
+++ modules/prompt/robot_api_prompt.py
@@ -1,9 +1,11 @@
 ROBOT_API = """
 def get_radius():
     '''
-    Get the radius of the robot.
+    Description: Get the radius of the robot itself.
     Returns:
-    - float: The radius of the robot.
+    - float: The radius of the robot itself.
+    Usage:
+    radius = get_radius()
     '''
 
 def get_position():
@@ -22,15 +24,17 @@ def set_velocity(velocity):
     Input:
     - velocity (numpy.ndarray): The new velocity to set.
     Usage: 
-    velocity = [vx, vy]
+    velocity = np.array([vx, vy])
     set_velocity(velocity)
     '''
 
 def get_velocity():
     '''
-    Get the velocity of the robot.
+    Get the velocity of the robot itself
     Returns:
     - numpy.ndarray: The velocity of the robot.
+    Usage:
+    vel = get_velocity()
     '''
 
 def get_surrounding_robots_info():
@@ -41,6 +45,12 @@ def get_surrounding_robots_info():
         - position (numpy.ndarray): The position of the robot.
         - velocity (numpy.ndarray): The velocity of the robot.
         - radius (float): The radius of the robot.
+    Usage:
+    robots_info = get_surrounding_robots_info()
+    for robot_info in robots_info:
+        pos = robot_info['position']
+        vel = robot_info['velocity']
+        radius = robot_info['radius']
     '''
 
 
@@ -51,20 +61,15 @@ def get_surrounding_obstacles_info():
     - list: A list of dictionaries, each containing the position and radius of an obstacle.
         - position (numpy.ndarray): The position of the obstacle.
         - radius (float): The radius of the obstacle.
-    '''
-""".strip()
-
-LEADER_API = """
-def get_leader_position():
-    '''
-    Get the position of the leader robot.
-
-    Returns:
-    - numpy.ndarray: The position of the leader robot.
+    Usage:
+    obstacles_info = get_surrounding_obstacles_info()
+    for obstacle_info in obstacles_info:
+        pos = obstacle_info['position']
+        radius = obstacle_info['radius']
     '''
 """.strip()
 
 
 def get_robot_api(leader=False):
     if leader:
-        return ROBOT_API + LEADER_API
+        return ROBOT_API
