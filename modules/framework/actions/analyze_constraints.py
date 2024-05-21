3c89e5c8ed98a00ac5d2dd363c33075f61f3b132
derrick <2287924124@qq.com>
2024-05-21 14:00:10 +0800

    feat: add interaction mode to analyze_constraints

--- modules/framework/actions/analyze_constraints.py
+++ modules/framework/actions/analyze_constraints.py
@@ -1,9 +1,12 @@
 import json
 
 from modules.framework.action import ActionNode
+from modules.llm.gpt import GPT
 from modules.prompt.analyze_stage_prompt import (
     ANALYZE_CONSTRAINT_PROMPT_TEMPLATE,
     CONSTRAIN_TEMPLATE,
+    CONTINUE_ANALYZE_CONSTRAINT_PROMPT_TEMPLATE,
+
 )
 from modules.prompt.robot_api_prompt import ROBOT_API
 from modules.prompt.env_description_prompt import ENV_DES
@@ -17,6 +20,13 @@ from modules.framework.response import *
 class AnalyzeConstraints(ActionNode):
     def __init__(self, next_text, node_name=""):
         super().__init__(next_text, node_name)
+        self._interaction_mode = False
+        if (hasattr(self.context.args, "interaction_mode")
+                and self.context.args.interaction_mode is True):
+            self.__llm = GPT(memorize=True)
+            self._interaction_mode = True
+        else:
+            self.__llm = GPT()
         self._constraint_pool: ConstraintPool = ConstraintPool()
 
     def _build_prompt(self):
@@ -31,10 +41,40 @@ class AnalyzeConstraints(ActionNode):
             user_constraints=json.dumps(user_constraints, indent=4),
         )
 
-    def _process_response(self, response: str) -> str:
+    async def _process_response(self, response: str) -> str:
         content = parse_text(response, "json")
         self._constraint_pool.init_constraints(content)
+        if self._interaction_mode:
+            satisfied = input("Are you satisfied with the constraints? (y/n)")
+            if satisfied != "y":
+                await self.interaction_with_user()
         logger.log(f"Analyze Constraints Success", "success")
 
+    async def interaction_with_user(self):
+        feedback = input("Please provide feedback:")
+        self.prompt = CONTINUE_ANALYZE_CONSTRAINT_PROMPT_TEMPLATE.format(
+            feedback=feedback,
+            output_template=CONSTRAIN_TEMPLATE,
+        )
+        await self._run()
 
-# if __name__ == '__main__':
+
+if __name__ == '__main__':
+    import asyncio
+    from modules.framework.context.workflow_context import WorkflowContext
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Run simulation with custom parameters."
+    )
+
+    parser.add_argument(
+        "--interaction_mode", type=bool, default=True, help="Whether to run in interaction mode in analyze constraints."
+    )
+    context = WorkflowContext()
+    context.command = "Integrate into a flock, adhering to cohesion by staying connected, alignment by moving together, and separation by maintaining at least 0.5 meters between robots."
+    args = parser.parse_args()
+    context.args = args
+    constraint_analyser = AnalyzeConstraints("analyze constraints")
+
+    asyncio.run(constraint_analyser.run())
