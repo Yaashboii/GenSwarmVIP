085cce29799197820a9e13053c78e70336dd1beb
MiangChen <2594416505@qq.com>
2024-07-21 20:16:44 +0800

    delete useless functions

--- modules/deployment/gymnasium_env/gymnasium_classification_env.py
+++ modules/deployment/gymnasium_env/gymnasium_classification_env.py
@@ -46,49 +46,6 @@ class GymnasiumClassificationEnvironment(GymnasiumEnvironmentBase):
         leader = Leader(leader_id=entity_id, initial_position=(0, 0), size=0.15)
         self.add_entity(leader)
 
-    @staticmethod
-    def find_farthest_points(points):
-        points = np.array(points)
-        distances = np.linalg.norm(points[:, np.newaxis] - points, axis=2)
-        farthest_indices = np.argmax(distances, axis=1)
-        return points[farthest_indices]
-
-    @staticmethod
-    def sample_points_on_circle(radius, center, num_points):
-        angles = np.linspace(0, 2 * np.pi, num_points, endpoint=False)
-        points = [(center[0] + radius * np.cos(angle),
-                   center[1] + radius * np.sin(angle)) for angle in angles]
-        return points
-
-    @staticmethod
-    def sample_points_inside_circle(radius, center, num_points, min_distance, max_attempts_per_point=10):
-        def distance(p1, p2):
-            return np.sqrt(np.sum((p1 - p2) ** 2, axis=1))
-
-        points = []
-        center = np.array(center)
-        attempts = 0
-
-        radius -= min_distance
-        while len(points) < num_points and attempts < num_points * max_attempts_per_point:
-            # Generate random points within the bounding square
-            random_points = np.random.uniform(-radius, radius, size=(num_points, 2)) + center
-            valid_mask = np.linalg.norm(random_points - center, axis=1) <= radius
-            random_points = random_points[valid_mask]
-
-            for point in random_points:
-                if len(points) == 0 or np.all(distance(np.array(points), point) >= min_distance):
-                    points.append(point)
-                    if len(points) >= num_points:
-                        break
-
-            attempts += 1
-
-        if len(points) < num_points:
-            print(f"Warning: Could only place {len(points)} points out of {num_points} requested.")
-
-        return np.array(points)
-
 
 if __name__ == "__main__":
 
