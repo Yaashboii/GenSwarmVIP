32f850f88fa0ed442239e3362fe5fe5fde1c4452
derrick <2287924124@qq.com>
2024-07-24 15:24:18 +0800

    feat: update metrics for cross experiment

--- run/auto_runner.py
+++ run/auto_runner.py
@@ -22,7 +22,7 @@ class AutoRunner:
         self.env_config_path = env_config_path
         self.experiment_path = workspace_path
         self.experiment_duration = experiment_duration
-        self.env = GymnasiumCrossEnvironment(self.env_config_path)
+        self.env = GymnasiumCrossEnvironment(self.env_config_path, radius=2.20)
         self.env.reset()
         self.results = {}
         self.manager = Manager(self.env, max_speed=max_speed)
@@ -46,12 +46,10 @@ class AutoRunner:
         return directories
 
     def experiment_completed(self, path):
-        # 检查是否已经存在 JSON 文件
         result_file = os.path.join(path, 'result.json')
         return os.path.exists(result_file)
 
     def save_experiment_result(self, path, result, analysis):
-        # 保存结果为 JSON 文件
         def convert_to_serializable(obj):
             if isinstance(obj, np.ndarray):
                 return obj.tolist()
@@ -105,48 +103,98 @@ class AutoRunner:
 
     def analyze_result(self, run_result):
         def are_trajectories_equal_length(data):
-            # 检查所有 trajectory 长度是否相等
             lengths = [len(info["trajectory"]) for info in data.values()]
             return all(length == lengths[0] for length in lengths)
 
         def collision_check(data, tolerance=0.1):
-            # 碰撞检查的逻辑
+            target_entities = {id: info for id, info in data.items() if info["target"] is not None}
+
             num_timesteps = len(next(iter(data.values()))["trajectory"])
-            for t in range(num_timesteps):
-                for id1, info1 in data.items():
+            collision_count = 0
+            collision_severity_sum = 0
+
+            for id1, info1 in target_entities.items():
+                for t in range(num_timesteps):
+                    pos1 = info1["trajectory"][t]
                     for id2, info2 in data.items():
-                        if id1 >= id2:
+                        if id1 == id2:
                             continue
-                        pos1 = info1["trajectory"][t]
                         pos2 = info2["trajectory"][t]
                         distance = math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
                         if distance + tolerance < (info1["size"] + info2["size"]):
-                            return True
-            return False
+                            collision_count += 1
+                            overlap = info1["size"] + info2["size"] - distance
+                            collision_severity_sum += overlap / (info1["size"] + info2["size"])
+
+            num_target_entities = len(target_entities)
+            collision_frequency = collision_count / (
+                    num_target_entities * num_timesteps) if num_target_entities > 0 else 0
+            collision_severity_mean = collision_severity_sum / collision_count if collision_count > 0 else 0
+
+            return collision_count > 0, collision_frequency, collision_severity_mean, collision_severity_sum
 
         def target_achieve(data, tolerance=0.1):
-            # 检查是否在目标点或附近
+            total_distance_ratio = 0
+            achieved_targets = 0
+            num_targets = 0
+            total_steps_ratio = 0
+
             for entity_id, info in data.items():
                 target = info["target"]
                 if target is None:
                     continue
-                final_position = info["trajectory"][-1]
-                distance = math.sqrt((target[0] - final_position[0]) ** 2 + (target[1] - final_position[1]) ** 2)
-                if distance > tolerance:
-                    return False
-            return True
+                num_targets += 1
+                initial_position = info["trajectory"][0]
+                initial_distance = math.sqrt(
+                    (target[0] - initial_position[0]) ** 2 + (target[1] - initial_position[1]) ** 2)
+
+                steps_to_target = len(info["trajectory"])
+                final_distance = initial_distance
+                for t, position in enumerate(info["trajectory"]):
+                    current_distance = math.sqrt(
+                        (target[0] - position[0]) ** 2 + (target[1] - position[1]) ** 2)
+                    if current_distance <= tolerance:
+                        steps_to_target = t + 1
+                        final_distance = current_distance
+                        break
+                if final_distance <= tolerance:
+                    achieved_targets += 1
+                    total_steps_ratio += steps_to_target / len(info["trajectory"])
+                distance_ratio = final_distance / initial_distance if initial_distance > 0 else 1
+                total_distance_ratio += distance_ratio
+
+                print(
+                    f"Entity {entity_id}: Initial Distance: {initial_distance}, Final Distance: {final_distance}, Ratio: {distance_ratio}, Steps to Target: {steps_to_target}")
+
+            target_achieve_ratio = achieved_targets / num_targets if num_targets > 0 else 0
+            average_distance_ratio = total_distance_ratio / num_targets if num_targets > 0 else 0
+            average_steps_ratio = total_steps_ratio / achieved_targets if achieved_targets > 0 else 'inf'
+
+            return achieved_targets == num_targets, target_achieve_ratio, average_distance_ratio, average_steps_ratio
 
         if not are_trajectories_equal_length(run_result):
             raise ValueError("Trajectories have different lengths")
 
+        collision, collision_frequency, collision_severity_mean, collision_severity_sum = collision_check(
+            run_result, tolerance=self.tolerance)
+        target_achieve, target_achieve_ratio, average_distance_ratio, average_steps_ratio = target_achieve(
+            run_result, tolerance=2 * self.tolerance)
+
         return {
-            'collision': collision_check(run_result, tolerance=self.tolerance),
-            'target_achieve': target_achieve(run_result, tolerance=self.tolerance)
+            'collision_occurred': collision,
+            'collision_frequency': collision_frequency,
+            'collision_severity_average': collision_severity_mean,
+            'collision_severity_sum': collision_severity_sum,
+            'target_achieved': target_achieve,
+            'target_achievement_ratio': target_achieve_ratio,
+            'distance_ratio_average': average_distance_ratio,
+            'steps_ratio_average': average_steps_ratio
         }
 
     def run_and_analyze_experiment(self, experiment):
         result = self.run_single_experiment(experiment)
         analysis = self.analyze_result(result)
+
         print(f"Analysis for experiment {experiment}: {analysis}")
         self.results[experiment] = analysis
         self.save_experiment_result(os.path.join(f"../workspace/{self.experiment_path}", experiment), result, analysis)
@@ -159,12 +207,10 @@ class AutoRunner:
         try:
             result = subprocess.run(command, timeout=self.experiment_duration - 2, capture_output=True, text=True,
                                     check=True)
-            # print(result.stdout)
         except subprocess.TimeoutExpired:
             print(f"\nExperiment {experiment} timed out and was terminated.")
         except subprocess.CalledProcessError as e:
             print(f"An error occurred while running {experiment}: {e}")
-            # print(f"Output: {e.stdout}")
             print(f"Errors: {e.stderr}")
 
     def run_multiple_experiments(self):
@@ -201,12 +247,28 @@ class AutoRunner:
 
         print("All experiments completed successfully.")
 
+    def plot_and_print_results(self, data, labels, ylabel, title, colors, save_filename):
+        plt.figure(figsize=(10, 6))
+        plt.bar(labels, data, color=colors)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        for i, v in enumerate(data):
+            plt.text(i, v + 0.01, f"{v:.2f}", ha='center')
+        plt_path = os.path.join(f"../workspace/{self.experiment_path}", save_filename)
+        plt.savefig(plt_path)
+        plt.show()
+        print(f"{title}: {data}")
+
     def analyze_all_results(self):
         experiment_dirs = self.get_experiment_directories()
-        collision_count = 0
-        target_achieve_count = 0
-        no_collision_and_target_achieve_count = 0
-        total_experiments = len(experiment_dirs)
+        collision_files = []
+        no_target_achieve_files = []
+        no_collision_and_target_achieve = []
+        collision_frequencies = []
+        collision_severities = []
+        distance_ratios = []
+        target_achievement_ratios = []
+        steps_ratios = []
 
         for experiment in experiment_dirs:
             result_path = os.path.join(f"../workspace/{self.experiment_path}", experiment, 'result.json')
@@ -214,37 +276,53 @@ class AutoRunner:
                 with open(result_path, 'r') as f:
                     result_data = json.load(f)
                     analysis = result_data.get('analysis', {})
-                    if analysis.get('collision') is False:
-                        collision_count += 1
-                    if analysis.get('target_achieve') is True:
-                        target_achieve_count += 1
-                    if analysis.get('collision') is False and analysis.get('target_achieve') is True:
-                        no_collision_and_target_achieve_count += 1
+                    if analysis.get('collision_occurred'):
+                        collision_files.append(experiment)
+                    if not analysis.get('target_achieved'):
+                        no_target_achieve_files.append(experiment)
+                    if not analysis.get('collision_occurred') and analysis.get('target_achieved'):
+                        no_collision_and_target_achieve.append(experiment)
 
-        collision_rate = collision_count / total_experiments if total_experiments else 0
-        target_achieve_rate = target_achieve_count / total_experiments if total_experiments else 0
-        no_collision_and_target_achieve_rate = no_collision_and_target_achieve_count / total_experiments if total_experiments else 0
+                    collision_frequencies.append(analysis.get('collision_frequency'))
+                    collision_severities.append(analysis.get('collision_severity_sum'))
+                    distance_ratios.append(analysis.get('distance_ratio_average'))
 
-        # Plotting the results
-        labels = ['No Collision Rate', 'Target Achieve Rate', 'No Collision and Target Achieve Rate']
-        rates = [collision_rate, target_achieve_rate, no_collision_and_target_achieve_rate]
+                    target_achievement_ratios.append(analysis.get('target_achievement_ratio'))
+                    if analysis.get('steps_ratio_average') != 'inf':
+                        steps_ratios.append(analysis.get('steps_ratio_average'))
 
-        plt.figure(figsize=(10, 6))
-        plt.bar(labels, rates, color=['red', 'green', 'blue'])
-        plt.ylabel('Rate')
-        plt.title('Experiment Analysis Rates')
-        plt.ylim(0, 1)
+        mean_collision_frequency = np.mean(collision_frequencies) if collision_frequencies else 0
+        mean_collision_severity = np.mean(collision_severities) if collision_severities else 0
+        mean_distance_ratio = np.mean(distance_ratios) if distance_ratios else 0
+        mean_target_achieve_ratio = np.mean(target_achievement_ratios) if target_achievement_ratios else 0
+        mean_steps_ratios = np.mean(steps_ratios) if steps_ratios else 0
+        metrics = [
+            ([mean_collision_frequency, mean_collision_severity], ['Collision Frequency*1000', 'Collision Severity'],
+             'Value', 'Collision Metrics', ['purple', 'orange'], 'collision_metrics.png'),
+            ([mean_distance_ratio, mean_target_achieve_ratio, mean_steps_ratios],
+             ['Distance Ratio', 'Target Achieve Ratio', 'Steps Ratio'], 'Ratio',
+             'Distance Metrics', ['cyan', 'blue', 'green'], 'distance_metrics.png'),
+        ]
 
-        for i, rate in enumerate(rates):
-            plt.text(i, rate + 0.01, f"{rate:.2f}", ha='center')
+        for data, labels, ylabel, title, colors, filename in metrics:
+            self.plot_and_print_results(data, labels, ylabel, title, colors, filename)
 
-        plt_path = os.path.join(f"../workspace/{self.experiment_path}", 'experiment_analysis.png')
-        plt.savefig(plt_path)
-        plt.show()
-
-        print(f"Collision Rate: {collision_rate:.2f}")
-        print(f"Target Achieve Rate: {target_achieve_rate:.2f}")
-        print(f"No Collision and Target Achieve Rate: {no_collision_and_target_achieve_rate:.2f}")
+        collision_rate = len(collision_files) / len(experiment_dirs) if experiment_dirs else 0
+        target_achieve_rate = len(no_target_achieve_files) / len(experiment_dirs) if experiment_dirs else 0
+        no_collision_and_target_rate = len(no_collision_and_target_achieve) / len(
+            experiment_dirs) if experiment_dirs else 0
+        self.plot_and_print_results(
+            [collision_rate, target_achieve_rate, no_collision_and_target_rate],
+            ['No Collision', 'Target Achieve', 'No Collision & Target Achieve'],
+            'Rate',
+            'Experiment Outcomes',
+            ['red', 'green', 'blue'],
+            'experiment_outcomes.png'
+        )
+        collision_files = '\n'.join(collision_files)
+        no_target_achieve_files = '\n'.join(no_target_achieve_files)
+        print(f"Experiments with collisions:\n{collision_files}")
+        print(f"\nExperiments without target achievement:\n{no_target_achieve_files}")
 
 
 if __name__ == "__main__":
@@ -252,7 +330,7 @@ if __name__ == "__main__":
                         workspace_path='layer/cross',
                         experiment_duration=40,
                         run_mode='analyze',
-                        max_speed=1.0,
+                        max_speed=4.0,
                         tolerance=0.05
                         )
 
