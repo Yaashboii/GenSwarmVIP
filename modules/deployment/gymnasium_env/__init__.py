a7899f08dffc86bb0ca205fc0bda0a004dd6883b
MiangChen <2594416505@qq.com>
2024-08-17 21:01:09 +0800

    update gymnasium_env init

--- modules/deployment/gymnasium_env/__init__.py
+++ modules/deployment/gymnasium_env/__init__.py
@@ -1,22 +1,24 @@
 # modules/deployment/gymnasium_env/__init__.py
 from .gymnasium_base_env import GymnasiumEnvironmentBase
-from .gymnasium_flocking_env import GymnasiumFlockingEnvironment
-from .gymnasium_collecting_env import GymnasiumCollectingEnvironment
-from .gymnasium_transportation_env import GymnasiumTransportationEnvironment
-from .gymnasium_herding_env import GymnasiumHerdingEnvironment
-from .gymnasium_crossing_env import GymnasiumCrossingEnvironment
 from .gymnasium_bridging_env import GymnasiumBridgingEnvironment
+from .gymnasium_circling_env import GymnasiumCirclingEnvironment
+from .gymnasium_collecting_env import GymnasiumCollectingEnvironment
 from .gymnasium_covering_env import GymnasiumCoveringEnvironment
+from .gymnasium_crossing_env import GymnasiumCrossingEnvironment
 from .gymnasium_exploration_env import GymnasiumExplorationEnvironment
+from .gymnasium_flocking_env import GymnasiumFlockingEnvironment
+from .gymnasium_encircling_env import GymnasiumEncirclingEnvironment
+from .gymnasium_transportation_env import GymnasiumTransportationEnvironment
 
 __all__ = [
     'GymnasiumBridgingEnvironment',
+    'GymnasiumCirclingEnvironment',
     'GymnasiumCollectingEnvironment',
     'GymnasiumCoveringEnvironment',
     'GymnasiumCrossingEnvironment',
     'GymnasiumEnvironmentBase',
     'GymnasiumExplorationEnvironment',
     'GymnasiumFlockingEnvironment',
-    'GymnasiumHerdingEnvironment',
+    'GymnasiumEncirclingEnvironment',
     'GymnasiumTransportationEnvironment',
 ]
