1f08f6cb748261ca58bca786495fef00bf7968f2
MiangChen <2594416505@qq.com>
2024-08-19 19:50:16 +0800

    rename response to parser

---
+++ modules/framework/parser/__init__.py
@@ -0,0 +1,7 @@
+from .code_parser import CodeParser
+from .text_parser import parse_text
+
+__all__ = [
+    'CodeParser',
+    'parse_text',
+]
--- modules/framework/response/__init__.py
+++
@@ -1,7 +0,0 @@
-from .code_parser import CodeParser
-from .text_parser import parse_text
-
-__all__ = [
-    'CodeParser',
-    'parse_text',
-]
